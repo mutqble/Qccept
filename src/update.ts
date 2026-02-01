c62004daea0f30c69dfe10cc15a091471bc3852b
noobnuby <me@noobnuby.com>
2026-02-01 22:21:54 +0900

    fix: updater

--- src/update.ts
+++ src/update.ts
@@ -1,10 +1,6 @@
 import { relaunch } from "@tauri-apps/plugin-process";
 import { check } from "@tauri-apps/plugin-updater";
-import toast, { Toaster } from "solid-toast";
-
-async function delay(ms: number) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-}
+import toast from "solid-toast";
 
 export async function update() {
     const update = await check();
@@ -13,14 +9,14 @@ export async function update() {
             `새로운 버전 ${update.version}이(가) 발견됐습니다.`,
         );
 
-        let promise = update.downloadAndInstall;
-        toast.promise(promise(), {
+        const promise = update.downloadAndInstall();
+
+        toast.promise(promise, {
             loading: `${update.version} 다운로드 중...`,
             success: "업데이트 완료! 잠시 후 재시작됩니다.",
             error: "업데이트 실패",
         }, { position: "top-center" });
 
-        await delay(3000);
         await relaunch();
     }
 }
